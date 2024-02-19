21724f018c1f7bc7530e236c9c22de8d820cfe8d
Tiago S.C <72054311+Tiago-Silverio-da-Costa@users.noreply.github.com>
2024-02-19 18:06:20 -0300

    intial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="mt-5 ml-5">
+      <Button>Hello</Button>
+      <h1>Hello</h1>
+    </div>
+  );
+}

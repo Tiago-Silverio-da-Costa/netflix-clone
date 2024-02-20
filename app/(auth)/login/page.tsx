ac715c561cd5a8a1700309afe669ca38582de5c5
Tiago S.C <72054311+Tiago-Silverio-da-Costa@users.noreply.github.com>
2024-02-20 19:32:46 -0300

    home improved+++

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,14 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import GoogleIcon from "../../../public/google.svg";
-import { GithubIcon } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
+import GithubSignInButton from "@/app/components/GithubSigninButton";
+import GoogleSignInButton from "@/app/components/GoogleSigninButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/utils/auth";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+  const session = await getServerSession(authOptions)
+
+  if (session){
+    return redirect("/home")
+  }
+
   return (
     <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
-      <form>
+      <form method="post" action="/api/auth/signin">
         <h1 className="text-3xl font-semibold text-white">Log in</h1>
         <div className="space-y-4 mt-5">
           <Input
@@ -29,22 +37,8 @@ export default function Login() {
       </div>
 
       <div className="flex w-full justify-center items-center gap-x-3 mt-6">
-        <Button
-          variant="outline"
-          size="icon"
-        >
-          <GithubIcon className="w-4 h-4" />
-        </Button>
-        <Button
-          variant="outline"
-          size="icon"
-        >
-          <Image
-            src={GoogleIcon}
-            alt="google icon"
-            className="w-6 h-6"
-          />
-        </Button>
+        <GithubSignInButton />
+        <GoogleSignInButton />
       </div>
     </div>
   )
---
+++ app/components/RecentlyAdded.tsx
@@ -0,0 +1,66 @@
+import prisma from "../utils/db"
+import Image from "next/image"
+import { MovieCard } from "./MovieCard"
+
+async function getData() {
+  const data = await prisma.movie.findMany({
+    select: {
+      id: true,
+      overview: true,
+      title: true,
+      WatchLits: true,
+      imageString: true,
+      youtubeString: true,
+      age: true,
+      release: true,
+      duration: true
+    },
+    orderBy: {
+      createdAt: "desc"
+    },
+    take: 4,
+  })
+
+  return data
+}
+
+export default async function RecentlyAdded() {
+  const data = await getData()
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 gap-6">
+      {data.map((movie) => (
+        <div key={movie.id} className="relative h-48">
+          <Image
+            src={movie.imageString}
+            alt="Movie"
+            width={500}
+            height={500}
+            className="rounded-sm absolute w-full h-full object-cover" />
+          <div className="h-60 relative z-10 w-full transform transition duration-500 hover:scale-125 opacity-0 hover:opacity-100">
+            <div className="bg-gradient-to-b from-transparent via-black/50 to-black z-10 w-full h-full rounded-lg flex justify-center items-center border">
+              <Image
+                src={movie.imageString}
+                alt="Movie"
+                width={800}
+                height={800}
+                className="absolute w-full h-full -z-10 rounded-lg object-cover" />
+              <MovieCard
+                movieId={movie.id}
+                overview={movie.overview}
+                title={movie.title}
+                watchListId={movie.WatchLits[0]?.id}
+                youtbeUrl={movie.youtubeString}
+                watchList={movie.WatchLits.length > 0 ? true : false}
+                key={movie.id}
+                age={movie.age}
+                time={movie.duration}
+                year={movie.release}
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
